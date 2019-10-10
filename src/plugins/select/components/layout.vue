fd2048882bbfb5e33b0b25fd1e8c813eb93b1ba2
desislavsd <n.nosme@gmail.com>
2019-10-10 11:30:35 +0300

    Added layout.vue

---
+++ src/plugins/select/components/layout.vue
@@ -0,0 +1,27 @@
+<template functional>
+    <div class="v-select" v-bind="data.attrs" v-on="listeners">
+        
+        <div class="v-select-bar">
+            <slot name="selected"></slot>
+
+            <slot></slot>
+        </div>
+
+        <div class="v-select-list" ref="list">
+
+            <slot name="options"></slot>
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    inject: ['select']
+}
+</script>
+
+<style>
+
+</style>
